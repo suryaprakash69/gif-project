3043fe82a26d8a9703ad275fc26911f934d2b1cf
suryaprakash69 <105296458+suryaprakash69@users.noreply.github.com>
2022-05-11 23:59:18 +0530

    gif-project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import "./App.css";
+import Giphy from './components/Giphy';
+
+const App = ()=> {
+    return(
+<div>
+    <Giphy/>
+</div>
+    );
+};
+
+export default App;
